745bb8911f18ae4702a7f43bde7ea68b37553b8c
root <root@ubuntu.(none)>
2016-06-02 00:32:48 +0530

    Vertical model updated

--- application/modules/admin/models/vertical_model.php
+++ application/modules/admin/models/vertical_model.php
@@ -1,60 +1,56 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
+if(!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
-
-class Vertical_model extends CI_Model {
+class Vertical_model extends CI_Model
+{
 
     function __construct()
     {
-        parent::__construct();
+	parent::__construct();
     }
-    
+
     function add_vertical()
     {
-    		 
-			$vertical_details = array(
-	            'vertical_name' => $this->input->post('vertical_name'),	             
-	            );
-	                                    
-	       	 return $this->db->insert('vertical',$vertical_details);
-		 
-		  
-	}
-	
-	function update_vertical()
+
+	$vertical_details = array(
+	    'vertical_name' => $this->input->post('vertical_name'),
+	);
+
+	return $this->db->insert('vertical', $vertical_details);
+    }
+
+    function update_vertical()
     {
-    	$vertical_details = array(
-	            'vertical_name' => $this->input->post('vertical_name'),	             
-	            );
-	           
-		 return $this->db->update('vertical',$vertical_details,array('id' => $this->input->post('vertical_id')));
-	}
-    
-	function vertical_list()
+	$vertical_details = array(
+	    'vertical_name' => $this->input->post('vertical_name'),
+	);
+
+	return $this->db->update('vertical', $vertical_details, array('id' => $this->input->post('vertical_id')));
+    }
+
+    function vertical_list()
+    {
+	$this->db->order_by("id", "desc");
+	$this->db->from('vertical');
+
+	return $this->db->get()->result();
+    }
+
+    function get_vertical($vertical_id)
+    {
+	return $this->db->get_where('vertical', array('id' => $vertical_id))->row();
+    }
+
+    function delete($category_id)
+    {
+	if($this->db->delete('vertical', array('id' => $category_id)))  // Delete customer
 	{
-		$this->db->order_by("id", "desc");		
-        $this->db->from('vertical');
-         
-        return $this->db->get()->result();
-	} 
-	
-    function get_vertical( $vertical_id )
-	{
-		return $this->db->get_where('vertical',array('id' => $vertical_id))->row();
+	    return true;
 	}
-	
-	function delete( $category_id )
-	{
-		if( $this->db->delete('vertical',array('id' => $vertical_id)) )  // Delete customer
-		{  
-			return true;
-		}
-	}
-     
-	  
+    }
 
 }
-
-
 
 ?>
